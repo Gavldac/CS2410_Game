53d6379288e2ae3939c0202db6136848369ee544
montemclaws <monte.mclaws@gmail.com>
2023-12-05 19:21:27 -0700

    Key Hud rough draft
    working but not polished.

--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour
 {
-    public GameObject key, gameOver;
+    public GameObject Key1,Key2,Key3, gameOver;
+    public GameObject HUDKey1, HUDKey2, HUDKey3;
+    public List<GameObject> KeyList; 
+    public List<GameObject> HUDKeyList;
+    public int index = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HUDKeyList = new List<GameObject> { HUDKey1,HUDKey2,HUDKey3 };
+        KeyList = new List<GameObject> { Key1, Key2, Key3 };
+
+       foreach(GameObject HUDKey in HUDKeyList)
+        {
+            HUDKey.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -20,9 +30,11 @@ public class KeyScript : MonoBehaviour
     {
         if(collision.gameObject.name == "Player")
         {
-            KeyCanvasScript.key.Add(key);
-            key.SetActive(false);
+            KeyCanvasScript.key.Add(KeyList[index]);
+            KeyList[index].SetActive(false);
             PortalScript.keys += 1;
+            HUDKeyList[index].SetActive(true);
+            index++;
         }
     }
 }
